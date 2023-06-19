57d028cb30796a66e2071310b1f7d8ab5c0e05f8
HSUxAJ <79509075+HSUxAJ@users.noreply.github.com>
2023-06-19 12:38:11 +0800

    update

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,12 +2,34 @@
   <div>
     <h2>Home</h2>
     <p>Welcome to the Home page!</p>
+    <button @click="goToJP">Go to JP</button>
+    <button @click="goToCNHKMO">Go to CN-HK-MO</button>
+    <button @click="goToEURO">Go to EURO</button>
+    <button @click="goToUSCA">Go to US-CA</button>
+    <button @click="goToMX">Go to MX</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HomePage'
+  name: 'HomePage',
+  methods: {
+    goToJP() {
+      this.$router.push({ name: 'JP' });
+    },
+    goToCNHKMO() {
+      this.$router.push({ name: 'CN-HK-MO' });
+    },
+    goToEURO() {
+      this.$router.push({ name: 'EURO' });
+    },
+    goToUSCA() {
+      this.$router.push({ name: 'US-CA' });
+    },
+    goToMX() {
+      this.$router.push({ name: 'MX' });
+    }
+  }
 };
 </script>
 
